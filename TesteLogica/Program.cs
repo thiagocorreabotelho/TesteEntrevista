16d581e1322fc0e0de5e09685ed779866927edcf
Thiago Corrêa <99252640+thiagocorreabotelho@users.noreply.github.com>
2022-11-17 21:49:28 -0300

    main: subindo finalização da avaliação

---
+++ TesteLogica/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace TesteLogica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variáveis de escopo
+
+            var listaOriginal = new ArrayList() { 7, 5, 3, 9, 6, 4, 1 };
+            var novaLista = listaOriginal;
+            int soma = 0;
+
+            #endregion
+
+            #region Exibição da lista original
+
+            for (int i = 0; i < listaOriginal.Count; i++)
+            {
+                Console.WriteLine($@"Lista original é {listaOriginal[i]} ");
+            }
+
+            #endregion
+          
+            // Apenas para pular de linha
+            Console.WriteLine();
+
+            #region Calculo da nova lista
+
+            for (int i = 0; i < novaLista.Count; i++)
+            {
+                var retorno = Convert.ToInt32(novaLista[i]);
+
+                if (retorno == 9)
+                    novaLista[i] = 5;
+
+                if (retorno == 4)
+                    novaLista.RemoveAt(i);
+
+                soma += Convert.ToInt32(novaLista[i]);
+
+                Console.WriteLine($@"Lista para substituir é {novaLista[i]}");
+            }
+
+            #endregion
+
+            // Apenas para pular de linha
+            Console.WriteLine();
+
+            Console.WriteLine($"O resultado da somatória da nova lista é de {soma}");
+        }
+    }
+}
---
+++ TesteLogica2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace TesteLogica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variáveis de escopo
+
+            var listaArray = new ArrayList() { 2, 7, 11, 15 };
+            int soma = 0;
+
+            #endregion
+
+            for (int i = 0; i < listaArray.Count; i++)
+            {
+                var retorno = Convert.ToInt32(listaArray[i]);
+
+                if (retorno == 7 || retorno == 15)
+                { 
+                    
+                    soma += Convert.ToInt32(listaArray[i]);
+
+                    if (soma == 22)
+                    {
+                        Console.WriteLine($"A soma de 7 + 15 é {soma}");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"O número {listaArray[i]} não faz parte do calculo");
+                }
+
+            }
+        }
+    }
+}
